0ee46910d2b0eae600c551d87a32f1c992431399
jamesh21 <jamesh21@uw.edu>
2024-12-31 13:43:08 -0800

    initial setup of project

---
+++ app.js
@@ -0,0 +1,8 @@
+require("dotenv");
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
