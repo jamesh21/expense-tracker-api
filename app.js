93884cf822c3bc9ac1755a148c2567a3887f8b4e
jamesh21 <jamesh21@uw.edu>
2024-12-31 16:38:13 -0800

    added mongodb connection

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-require("dotenv");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const authRouter = require("./routes/auth");
 const expenseRouter = require("./routes/expense");
+const connectDB = require("./db/connect");
 // middleware
 app.use(express.json());
 
@@ -11,6 +12,15 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/expenses", expenseRouter);
 
-app.listen(port, () => {
-    console.log(`server is listening on port ${port}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`server is listening on port ${port}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+startServer();
